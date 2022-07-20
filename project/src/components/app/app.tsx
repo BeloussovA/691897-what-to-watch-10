e2c67c81c78e98157792d86ee8dc7f9ff50940f1
BeloussovA <andrey.belousov82@gmail.com>
2022-07-20 22:39:28 +0600

    Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,17 +1,51 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPage from '../../pages/main-page/main-page';
-// import SignIn from '../../pages/login/login';
-// import MyList from '../../pages/my-list/my-list';
+import SignIn from '../../pages/login/login';
+import MyList from '../../pages/my-list/my-list';
 // import Film from '../../pages/film/film';
 // import AddReview from '../../pages/add-review/add-review';
-// import Player from '../../pages/player/player';
+import Player from '../../pages/player/player';
 import { FilmSetting, FilmsData } from '../../utils/utils';
 
 function App() : JSX.Element {
   return (
-    <MainPage
-      filmSetting={FilmSetting}
-      filmsData={FilmsData}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage filmSetting={FilmSetting} filmsData={FilmsData}/>}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyList />}
+        />
+        {/* <Route
+          path={AppRoute.Film}
+          element={<GenreQuestionScreen />}
+        /> */}
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        {/* <Route
+          path={AppRoute.AddReview}
+          element={<WinScreen />}
+        /> */}
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        {/* <Route
+          path="*"
+          element={<NotFoundScreen />}
+        /> */}
+      </Routes>
+    </BrowserRouter>
+    // <MainPage
+    //   filmSetting={FilmSetting}
+    //   filmsData={FilmsData}
+    // />
     // <
     //   SignIn />
     // <
